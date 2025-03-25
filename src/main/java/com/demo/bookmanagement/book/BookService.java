762c6807f37c4e854e4122089beb85e578e8b62c
Adesh <adeshslad@gmail.com>
2025-03-25 12:31:29 +0530

    changed from UUID to Long

--- src/main/java/com/demo/bookmanagement/book/BookService.java
+++ src/main/java/com/demo/bookmanagement/book/BookService.java
@@ -3,8 +3,6 @@ package com.demo.bookmanagement.book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class BookService {
